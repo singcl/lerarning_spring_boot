f062317ce4e35f3f2cf490b5523b118c672ade4b
singcl <1276837689@qq.com>
2020-11-30 17:11:20 +0800

    Controller 写到一个文件中

--- src/main/java/com/singcl/learning/Controller/HelloController.java
+++ src/main/java/com/singcl/learning/Controller/HelloController.java
@@ -1,6 +1,5 @@
-package com.singcl.learning;
+package com.singcl.learning.Controller;
 
-import com.singcl.learning.Greeting;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,17 +7,12 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
-public class GreetingController {
+public class HelloController {
     private static final String temple = "\"Hello, %s!\";";
     private final AtomicLong counter = new AtomicLong();
 
     @GetMapping("/hello")
     public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
         return String.format("Hello %s", name);
-    }
-
-    @GetMapping("/greeting")
-    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name){
-        return new Greeting(counter.incrementAndGet(), String.format(temple, name));
     }
 }
