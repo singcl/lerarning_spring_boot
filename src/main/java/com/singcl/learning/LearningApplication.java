72207d60335e7e9bdeed818d1802bea771258f08
singcl <1276837689@qq.com>
2020-11-30 17:02:28 +0800

    新增一个controller

--- src/main/java/com/singcl/learning/LearningApplication.java
+++ src/main/java/com/singcl/learning/LearningApplication.java
@@ -2,20 +2,10 @@ package com.singcl.learning;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class LearningApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(LearningApplication.class, args);
-	}
-
-	@GetMapping("/hello")
-	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return String.format("Hello %s", name);
 	}
 }
