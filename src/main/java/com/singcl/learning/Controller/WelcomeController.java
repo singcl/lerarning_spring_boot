02b095bc47ed7360461cc6aba334e48bed0bff43
singcl <1276837689@qq.com>
2020-11-30 19:20:52 +0800

    添加index.html等前端页面

---
+++ src/main/java/com/singcl/learning/Controller/WelcomeController.java
@@ -0,0 +1,15 @@
+package com.singcl.learning.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class WelcomeController {
+    @GetMapping("/welcome")
+    public String welcome(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "welcome";
+    }
+}
