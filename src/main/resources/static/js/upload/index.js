d1abf6d105765249278f0c3c8d8cf499ed9e89fb
singcl <1276837689@qq.com>
2020-12-04 16:36:57 +0800

    上传成功，失败message提示

--- src/main/resources/static/js/upload/index.js
+++ src/main/resources/static/js/upload/index.js
@@ -15,6 +15,26 @@
             },
             handlePreview(file) {
                 console.log(file);
+            },
+            handleSuccess(response, file, fileList) {
+                console.log(response, file, fileList);
+                this.$message({
+                    message: '上传成功',
+                    type: 'success',
+                    duration: 1000,
+                    onClose: function() {
+                        window.location.reload();
+                    }
+                });
+
+            },
+            handleError(err, file, fileList) {
+                console.log(err, file, fileList)
+                this.$message({
+                    message: '上传失败',
+                    type: 'error',
+                    duration: 1000
+                });
             }
         }
     })
