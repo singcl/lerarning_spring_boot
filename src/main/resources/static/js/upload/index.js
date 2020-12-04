aec513b1444105e4add136948998812be6037370
singcl <1276837689@qq.com>
2020-12-04 12:01:03 +0800

    上传页面

--- src/main/resources/static/js/upload/index.js
+++ src/main/resources/static/js/upload/index.js
@@ -3,13 +3,7 @@
         template: '#uploadTemplate',
         data() {
             return {
-                fileList: [{
-                    name: 'food.jpeg',
-                    url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100'
-                }, {
-                    name: 'food2.jpeg',
-                    url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100'
-                }]
+                fileList: []
             };
         },
         methods: {
